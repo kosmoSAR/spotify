f8b8fb6194ce8b2905186545d1004fadd9d7e06a
kosmoSAR <santiagoaristizabal.kosmetikon@gmail.com>
2023-06-21 11:54:59 -0500

    finalizo la aplicación

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tarea } from 'src/app/models/tarea';
 
 @Component({
   selector: 'app-tareas',
   templateUrl: './tareas.component.html',
   styleUrls: ['./tareas.component.css']
 })
-export class TareasComponent {
 
+export class TareasComponent implements OnInit {
+
+  listaTareas: Tarea[]=[];
+  nombreTarea:string="";
+
+  constructor(){}
+
+  ngOnInit(): void {   
+  }
+
+  agregarTarea(){
+    const tarea:Tarea = {
+      nombre: this.nombreTarea,
+      estado: false,
+    }
+
+    this.listaTareas.push(tarea);
+    this.nombreTarea="";
+  }
+
+  eliminar(i:number){
+    this.listaTareas.splice(i,1);
+  }
+
+  actualizarTarea(tarea:Tarea, i:number){
+    this.listaTareas[i].estado = !tarea.estado;
+  }
 }
---
+++ src/app/models/tarea.ts
@@ -0,0 +1,9 @@
+export class Tarea{
+    nombre:string;
+    estado:boolean;
+
+    constructor(n:string, e:boolean){
+        this.nombre = n;
+        this.estado = e;
+    }
+}
